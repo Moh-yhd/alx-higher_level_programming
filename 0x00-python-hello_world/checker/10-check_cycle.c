87d49ebde68bdef8849c49be54941befcb4332bf
Mohammmed <mohammedthe11th@gmail.com>
2023-01-02 15:07:21 +0300

    efficiency

--- 0x00-python-hello_world/checker/10-check_cycle.c
+++ 0x00-python-hello_world/checker/10-check_cycle.c
@@ -5,32 +5,30 @@
  * check_cycle - checks if a link has a cycle or not
  * @list: is the head of the linked list
  *
- * Return: 0 if no cycle and 1 if ther is a cycle
+ * Return: 0 if there is no cycle and 1 if there is a cycle
  */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *temp1, *temp2, *check;
-	int i;
+	listint_t *temp1, *temp2;
+
+	if (list == NULL)
+	{
+		return (0);
+	}
 
 	temp1 = list;
+	temp2 = list;
 
-	while (temp1)
+	while (temp1 && temp2 && temp2->next)
 	{
-		check = temp1->next;
-		temp2 = list;
-		i = 0;
-		while (temp2)
-		{
-			if (check == temp2->next)
-			{
-				i++;
-			}
-			if (i > 1)
-				return (1);
-			temp2 = temp2->next;
-		}
 		temp1 = temp1->next;
+		temp2 = temp2->next->next;
+		
+		if (temp1 == temp2)
+		{
+			return (1);
+		}
 	}
 	return (0);
 }
