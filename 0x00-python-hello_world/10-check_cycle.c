092dd434fdb2950897ddf71b22c6b4cdc2b00aa3
Mohammmed <mohammedthe11th@gmail.com>
2023-01-02 14:44:06 +0300

    checking for a cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - checks if a link has a cycle or not
+ * @list: is the head of the linked list
+ *
+ * Return: 0 if no cycle and 1 if ther is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *temp1, *temp2, *check;
+	int i;
+
+	temp1 = list;
+
+	while (temp1)
+	{
+		check = temp1->next;
+		temp2 = list;
+		i = 0;
+		while (temp2)
+		{
+			if (check == temp2->next)
+			{
+				i++;
+			}
+			if (i > 1)
+				return (1);
+			temp2 = temp2->next;
+		}
+		temp1 = temp1->next;
+	}
+	return (0);
+}
+
