f67a93e5352625c2fbe3987658bce7a5552776b9
Mohammmed <mohammedthe11th@gmail.com>
2023-01-03 19:25:59 +0300

    combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i < j:
+            print("{}{}".format(i, j), end='')
+            if i == 8 and j == 9:
+                continue
+            print(", ", end='')
+print("")
