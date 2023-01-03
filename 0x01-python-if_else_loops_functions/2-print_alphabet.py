b1ff21a79846f5ec2dddbcfdf42be3c29cc29444
Mohammmed <mohammedthe11th@gmail.com>
2023-01-03 17:39:44 +0300

    ASCII

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(97, 122):
-    print(chr(i), end="")
+for i in range(97, 123):
+    print(f"{chr(i)}", end="")
     if i == 122:
-        print(chr(i))
         break
