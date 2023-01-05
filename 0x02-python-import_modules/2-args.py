b403fa9ca69ce99afeb0b569cb4283f3a363fc0b
Mohammmed <mohammedthe11th@gmail.com>
2023-01-05 20:31:38 +0300

    out put

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) > 2:
-        print("{:d} arguments:".format(len(sys.argv)))
+        print("{:d} arguments:".format(len(sys.argv) - 1))
     else:
         if len(sys.argv) == 1:
             print("{:d} argument.".format(len(sys.argv) - 1))
