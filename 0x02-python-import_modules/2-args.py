b18f8cb5bf461fa5c06519170a0cee9c025699f6
Mohammmed <mohammedthe11th@gmail.com>
2023-01-05 20:36:35 +0300

    proper call

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 2:
-        print("{:d} arguments:".format(len(sys.argv) - 1))
+    from sys import argv
+    if len(argv) > 2:
+        print("{:d} arguments:".format(len(argv) - 1))
     else:
-        if len(sys.argv) == 1:
-            print("{:d} argument.".format(len(sys.argv) - 1))
+        if len(argv) == 1:
+            print("{:d} argument.".format(len(argv) - 1))
         else:
-            print("{:d} argument:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv)):
+            print("{:d} argument:".format(len(argv) - 1))
+    for i in range(len(argv)):
         j = i + 1
-        if j == len(sys.argv):
+        if j == len(argv):
             break
-        print("{:d}: {}".format(j, sys.argv[j]))
+        print("{:d}: {}".format(j, argv[j]))
