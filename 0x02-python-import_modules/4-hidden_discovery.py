de2dd21c2232eb4310eb7d9f9551e11666846b13
Mohammmed <mohammedthe11th@gmail.com>
2023-01-05 22:31:35 +0300

    magic

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import imp
+    compiled_module = imp.load_compiled('hidden_4', 'hidden_4.pyc')
+    for name in compiled_mudule_dict__:
+        print(name)
