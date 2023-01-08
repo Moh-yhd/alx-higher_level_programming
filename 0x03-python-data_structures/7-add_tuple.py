c7ec16ae04ae0dc885c52ccc8e3f58c59db85197
Mohammmed <mohammedthe11th@gmail.com>
2023-01-09 01:28:49 +0300

    tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_sum = ()
+    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+        tuple_sum += (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    elif len(tuple_a) == 0:
+        return tuple_b
+    elif len(tuple_b) == 0:
+        return tuple_a
+    elif len(tuple_a) > len(tuple_b):
+        tuple_sum += (tuple_a[0] + tuple_b[0], tuple_a[1])
+    elif len(tuple_a) < len(tuple_b):
+        tuple_sum += (tuple_a[0] + tuple_b[0], tuple_b[1])
+    return tuple_sum
