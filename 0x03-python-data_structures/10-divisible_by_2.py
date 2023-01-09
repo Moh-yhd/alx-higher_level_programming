24d6f5ee3f779b0bdd2fbde4bb2e4087164296bc
Mohammmed <mohammedthe11th@gmail.com>
2023-01-09 14:07:09 +0300

    check divisible

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    check = []
+    i = 0
+    while  i < len(my_list):
+        if my_list[i] % 2 != 0:
+            check.append(False)
+        elif my_list[i] % 2 == 0:
+            check.append(True)
+        i += 1
+    return check
