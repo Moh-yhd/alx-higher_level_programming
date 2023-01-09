ea22ee08441f81208cbdf96b59737e627cea72a6
Mohammmed <mohammedthe11th@gmail.com>
2023-01-09 12:44:29 +0300

    edge case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return None
-    n = len(sentence)
-    c = sentence[0]
     tuple_return = ()
-    tuple_return += (n, c)
+    if len(sentence) == 0:
+        tuple_return += (0, None)
+    else:
+        n = len(sentence)
+        c = sentence[0]
+        tuple_return += (n, c)
     return tuple_return
