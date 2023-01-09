a45b677db95841e7ab48f56f91ba18a31458b56d
Mohammmed <mohammedthe11th@gmail.com>
2023-01-09 13:28:48 +0300

    maximum

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    maximum = my_list[0]
+    for i in range(len(my_list) - 1):
+        for j in range(len(my_list) - 1):
+            if my_list[j] > my_list[i]:
+                maximum = my_list[j]
+                return (maximum)
+    return maximum
