46bebf236f2e0dbefa14638871f1c3ffc644fbfa
Mohammmed <mohammedthe11th@gmail.com>
2023-01-09 22:57:18 +0300

    update

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -16,38 +16,42 @@ listint_t *insert_node(listint_t **head, int number)
 	temp1 = *head;
 	if (*head == NULL)
 		return (NULL);
-
+	temp2 = malloc(sizeof(listint_t));
+	if (temp2 == NULL)
+		return (NULL);
+	temp2->n = number;
+	temp2->next = NULL;
 	while (temp1)
 	{
 		if (temp1->n <= number && number <= temp1->next->n)
 		{
 			flag = 1;
+			current = temp1->next;
+			temp1->next = temp2;
+			temp2->next = current;
 			break;
 		}
 		temp1 = temp1->next;
 	}
 	if (flag == 0)
 	{
+		printf("loop entered\n");
 		temp1 = *head;
 		if ((temp1->n > temp1->next->n && temp1->n < number) ||
 				(temp1->n < temp1->next->n && temp1->n > number))
+		{
 			flag = 1;
+			temp2->next = *head;
+			*head = temp2;
+		}
 	}
 	if (flag == 0)
 	{
-		temp1 = *head;
-		while (temp1)
+		printf("loop entered\n");
+		while (temp1->next != NULL)
 			temp1 = temp1->next;
+		temp1->next = temp2;
 	}
-
-	temp2 = malloc(sizeof(listint_t));
-	if (temp2 == NULL)
-		return (NULL);
-	temp2->n = number;
-	temp2->next = NULL;
-	current = temp1->next;
-	temp1->next = temp2;
-	temp2->next = current;
 
 	return (*head);
 }
