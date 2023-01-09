85923aa8afccda1357f392b041aa90956acf78e9
Mohammmed <mohammedthe11th@gmail.com>
2023-01-09 16:27:20 +0300

    None added

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 int is_palindrome(listint_t **head)
 {
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if my_list is None or len(my_list) == 0:
         return None
     maximum = my_list[0]
     for i in range(len(my_list) - 1):
