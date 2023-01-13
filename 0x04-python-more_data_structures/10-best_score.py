106a0a17762fe2c65783772fcc32c1786f0cabd0
Mohammed_Android <mohammedthe11th@gmail.com>
2023-01-13 19:30:04 +0300

    max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,6 @@ def best_score(a_dictionary):
     if a_dictionary is not None:
         maximum = list(a_dictionary.values())[0]
         for key, value in a_dictionary.items():
-            
             if value > maximum:
                 maximum = value
         for key, value in a_dictionary.items():
