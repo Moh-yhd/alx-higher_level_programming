7f87dcbaca49f338d0f79b1eb97304839ccf42f0
Mohammmed <mohammedthe11th@gmail.com>
2023-01-30 22:49:23 +0300

    creating a class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """Creates a class Rectangle
+    Args:
+        None
+    Returns:
+        None
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This directory contains practice tasks on classes in python
