23a43a1943ae8bbdeb500f9e348ac93d285bbff4
Mohammed_Android <mohammedthe11th@gmail.com>
2023-01-30 14:20:41 +0300

    position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -41,7 +41,7 @@ class Square:
 
     @property
     def position(self):
-        """Gets the coordinates of the square
+        """Gets the position of the square
         Return:
             None
         """
@@ -55,12 +55,12 @@ class Square:
         Returns:
             None
         """
-        if type(value) is not tuple or len(value)
-        != 2 or type(value[0]) is not
-        int or type(value[1]) is not int:
+        if type(value) is not tuple or len(value) != 2\
+        or type(value[0]) is not int or type(value[1]) is not int or\
+        value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            self.__position = valueo
+            self.__position = value
 
     def area(self):
         """Calculates the area of a square and returns it"""
