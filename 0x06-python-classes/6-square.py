7c69ecfb2fb4b8b2d8ece27a3cdc7608e50b13ba
Mohammed_Android <mohammedthe11th@gmail.com>
2023-01-30 14:03:43 +0300

    adding position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,14 +7,14 @@ class Square:
     Attributes:
         __size (int): size of a side of a square
     """
-    def __init__(self, size=0):
-        """Validating the size of a square"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+    def __init__(self, size=0, position=(0, 0)):
+        """Sets attributes
+        Args:
+            size (int): thr size of the square
+            position (tuple) the coordinates of center of the square
+        """
+        self.__size = size
+        self.__position = position
 
     @property
     def size(self):
@@ -39,6 +39,29 @@ class Square:
         else:
             self.__size = value
 
+    @property
+    def position(self):
+        """Gets the coordinates of the square
+        Return:
+            None
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the coordinate position of a square
+        Args:
+            value (int): coordinate of the squaere
+        Returns:
+            None
+        """
+        if type(value) is not tuple or len(value)
+        != 2 or type(value[0]) is not
+        int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = valueo
+
     def area(self):
         """Calculates the area of a square and returns it"""
         return (self.__size)**2
@@ -52,6 +75,7 @@ class Square:
             print()
             return
         for i in range(self.__size):
+            print('_'*self.position[0], end="")
             for j in range(self.__size):
                 print('#', end="")
             print()
