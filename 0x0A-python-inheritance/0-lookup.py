ad2dd422391ee9b9b3e28897e2a265f4103bb229
Mohammmed <mohammedthe11th@gmail.com>
2023-02-08 23:07:08 +0300

    list of attrib and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Lookup module """
+
+
+def lookup(obj):
+    """ returns a list of all the attributes and methods of a list """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This directory contains practice tasks about inheritance in python
